5a25a3dc1850613eb17d9a7c3fe34f2101ae63db
Peh Zi Heng <pehziheng@gmail.com>
2020-09-04 20:03:22 +0800

    Initial Commit

---
+++ app/src/main/java/sg/edu/nyp/signquest/start/StartFragment.kt
@@ -0,0 +1,32 @@
+package sg.edu.nyp.signquest.start
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import sg.edu.nyp.signquest.R
+
+class StartFragment : Fragment() {
+
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+}
