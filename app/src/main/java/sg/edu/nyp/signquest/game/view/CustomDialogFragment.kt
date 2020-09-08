b18c1f0e9c779a2396bfc8a9f3b702d5e4ca1c5c
Xskullibur <alsonproj2222@gmail.com>
2020-09-08 20:12:12 +0800

    CustomDialogFragment.kt Completed

--- app/src/main/java/sg/edu/nyp/signquest/game/view/CustomDialogFragment.kt
+++ app/src/main/java/sg/edu/nyp/signquest/game/view/CustomDialogFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.fragment_custom_dialog.*
 import kotlinx.android.synthetic.main.fragment_custom_dialog.view.*
 import sg.edu.nyp.signquest.R
 
