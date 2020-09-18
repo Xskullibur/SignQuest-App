ca75c78a8be5a8fae8cc646b9767d81ef0342682
lee hsien xiang <leehsienx@gmail.com>
2020-09-18 21:13:51 +0800

    remove ml

--- app/src/main/java/sg/edu/nyp/signquest/SignLanguageImageAnalyzer.kt
+++ app/src/main/java/sg/edu/nyp/signquest/SignLanguageImageAnalyzer.kt
@@ -6,7 +6,6 @@ import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
-import sg.edu.nyp.signquest.ml.Christina
 import java.nio.ByteBuffer
 
 
@@ -15,7 +14,6 @@ class SignLanguageImageAnalyzer(val context: Context) : ImageAnalysis.Analyzer {
     private val TAG = SignLanguageImageAnalyzer::class.java.name
 
     private val charAlphabetRange = 'A'..'Z'
-    val model = Christina.newInstance(context)
 
     private fun ByteBuffer.toByteArray(): ByteArray {
         rewind()    // Rewind the buffer to zero
@@ -32,31 +30,6 @@ class SignLanguageImageAnalyzer(val context: Context) : ImageAnalysis.Analyzer {
 //        val data = buffer.toByteArray()
 
         //val bitmap = imageProxy.toBitmap()
-        val bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.dumb5)
-
-        //Resize image to 28x28
-        val resizeBitmap = toGrayscale(Bitmap.createScaledBitmap(bitmap!!, 28, 28, false))!!
-
-        // Creates inputs for reference.
-        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 28, 28, 1), DataType.FLOAT32)
-        val resizeBuffer = ByteBuffer.allocate(resizeBitmap.byteCount)
-
-        resizeBitmap.copyPixelsToBuffer(resizeBuffer)
-
-        val pixels = resizeBuffer.toByteArray().take(784).map { (it.toInt() and 0xFF) / 255 }.toIntArray()
-
-        inputFeature0.loadArray(pixels)
-
-        // Runs model inference and gets result.
-        val outputs = model.process(inputFeature0)
-        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
-
-        val results = outputFeature0.floatArray
-        val charIndex = results.indexOf(results.max()!!)
-        val predictedChar = charAlphabetRange.elementAt(charIndex)
-
-        println("Predicted Alphabet: $predictedChar")
-
 
 //        // Releases model resources if no longer used.
 //        model.close()
